8199aa32d9bee46df1f3517b30dbdb5f0671e70a
lennertamas <tamas.lenner@gmail.com>
2023-02-24 09:14:41 +0100

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.Add(3, 4));
+    }
+
+    public  int Add (int a, int b)
+    {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Epic("Epic 1")
+public class CalculatorTest {
+
+    @Test
+    @Story("User Story1")
+    @Description ("Description related to testAdd test")
+    @Severity(SeverityLevel.NORMAL)
+    @Tag("Tag 1")
+    @Link("https://example.org")
+    public void testAdd()
+    {
+        Calculator calc = new Calculator();;
+
+        Assertions.assertEquals(5, calc.Add(2, 3));
+    }
+
+
+    @Test
+    @Story("User Story2")
+    @Description ("Description related to testAddWithNegative test")
+    @Severity(SeverityLevel.BLOCKER)
+    @Tag("Tag 2")
+    public void testAddWithNegative()
+    {
+        Calculator calc = new Calculator();
+
+        Assertions.assertEquals(-5, calc.Add(-2,-3));
+    }
+}
